ec06032ab6a6a4a965bcedf452cc023c32ae56fc
EricaPereira34 <ericajspereira@gmail.com>
2026-06-15 23:34:24 -0300

    Implementada edicao de palpites

--- src/app/palpite-component/palpite-component.ts
+++ src/app/palpite-component/palpite-component.ts
@@ -39,18 +39,37 @@ export class PalpiteComponent implements OnInit {
   }
 
   salvar(): void {
-    this.service.save(this.palpite).subscribe({
+    if (this.palpite.id === 0) {
+      this.service.save(this.palpite).subscribe({
+        next: () => {
+          this.listarPalpites();
+          this.limparFormulario();
+        },
+        error: (erro) => {
+          console.log(erro);
+        }
+      });
+    } else {
+      this.service.update(this.palpite.id, this.palpite).subscribe({
+        next: () => {
+          this.listarPalpites();
+          this.limparFormulario();
+        },
+        error: (erro) => {
+          console.log(erro);
+        }
+      });
+    }
+  }
+
+  editar(palpite: Palpite): void {
+    this.palpite = { ...palpite };
+  }
+
+  excluir(id: number): void {
+    this.service.delete(id).subscribe({
       next: () => {
         this.listarPalpites();
-
-        this.palpite = {
-          id: 0,
-          participante: '',
-          timeA: '',
-          timeB: '',
-          golsTimeA: 0,
-          golsTimeB: 0
-        };
       },
       error: (erro) => {
         console.log(erro);
@@ -58,15 +77,14 @@ export class PalpiteComponent implements OnInit {
     });
   }
 
-  excluir(id: number): void {
-  this.service.delete(id).subscribe({
-    next: () => {
-      this.listarPalpites();
-    },
-    error: (erro) => {
-      console.log(erro);
-    }
-  });
-}
-
+  limparFormulario(): void {
+    this.palpite = {
+      id: 0,
+      participante: '',
+      timeA: '',
+      timeB: '',
+      golsTimeA: 0,
+      golsTimeB: 0
+    };
+  }
 }
